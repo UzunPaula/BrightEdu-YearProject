7d7b151f2de3921aae93b406a552d1801e973dad
UP <uzun.paula777@gmail.com>
2026-02-10 23:02:33 +0200

    implement Clean Architecture and demonstrate OOP + SOLID principles

--- BrightEdu.Web/Program.cs
+++ BrightEdu.Web/Program.cs
@@ -1,9 +1,32 @@
+using BrightEdu.Application.Features.Courses;
+using BrightEdu.Application.Features.Lessons;
+using BrightEdu.Application.Features.Lessons.Mapper;
+using BrightEdu.Application.Interfaces;
+using BrightEdu.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+// aici leg contractul la implementare (DIP(Dependency Inversion Principle) în practică)
+// Repositories
+builder.Services.AddScoped<ICourseReadRepository, InMemoryCourseRepository>();
+builder.Services.AddScoped<ICourseWriteRepository, InMemoryCourseRepository>();
+builder.Services.AddScoped<ILessonRepository, InMemoryLessonRepository>();
+
+// Services (use cases)
+builder.Services.AddScoped<IGetCoursesService, GetCoursesService>();
+builder.Services.AddScoped<IGetLessonService, GetLessonService>();
+
+// OCP: înregistrez mapper-ele, iar service-ul le folosește automat.
+// Mappers
+builder.Services.AddScoped<ILessonStepDtoMapper, ContentStepDtoMapper>();
+builder.Services.AddScoped<ILessonStepDtoMapper, QuestionStepDtoMapper>();
 
 var app = builder.Build();
 
@@ -16,29 +39,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
